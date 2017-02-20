dcfc3f1caa5da0f52a4c7de12288122c1e96d90b
Robert Oleksy <robertoleksy6@gmail.com>
2017-02-20 11:07:31 +0000

    connect_as_client in create descriptors

--- c_sockfd_manager.cpp
+++ c_sockfd_manager.cpp
@@ -5,8 +5,10 @@ int c_sockfd_manager::add_udp_descriptor(int domain, int type, int protocol) {
 	if (domain != AF_INET6) throw std::invalid_argument("domain is not AF_INET6");
 	if (type != SOCK_DGRAM) throw std::invalid_argument("type is not SOCK_DGRAM");
 	int descriptor = socket(domain, type, protocol);
-	if (descriptor != -1)
+	if (descriptor != -1) {
 		m_udp_descriptors.emplace(std::make_pair(descriptor, c_turbosocket()));
+		m_udp_descriptors.at(descriptor).connect_as_client();
+	}
 	return descriptor;
 }
 
@@ -14,8 +16,10 @@ int c_sockfd_manager::add_tcp_descriptor(int domain, int type, int protocol) {
 	if (domain != AF_INET6) throw std::invalid_argument("domain is not AF_INET6");
 	if (type != SOCK_STREAM) throw std::invalid_argument("type is not SOCK_STREAM");
 	int descriptor = socket(domain, type, protocol);
-	if (descriptor != -1)
+	if (descriptor != -1) {
 		m_tcp_descriptors.emplace(std::make_pair(descriptor, c_turbosocket()));
+		m_tcp_descriptors.at(descriptor).connect_as_client();
+	}
 	return descriptor;
 }
 
