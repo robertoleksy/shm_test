19b254ce44de48b940236c567e20eae6902c285c
Robert Oleksy <robertoleksy6@gmail.com>
2017-02-20 12:16:34 +0000

    [wip] use in example udp client

--- c_sockfd_manager.cpp
+++ c_sockfd_manager.cpp
@@ -1,10 +1,15 @@
 #include "c_sockfd_manager.hpp"
 #include <sys/socket.h>
+#include <dlfcn.h>
 
 int c_sockfd_manager::add_udp_descriptor(int domain, int type, int protocol) {
-	if (domain != AF_INET6) throw std::invalid_argument("domain is not AF_INET6");
+	std::cout << "add udp socket" << std::endl;
+	//if (domain != AF_INET6) throw std::invalid_argument("domain is not AF_INET6");
 	if (type != SOCK_DGRAM) throw std::invalid_argument("type is not SOCK_DGRAM");
-	int descriptor = socket(domain, type, protocol);
+	int(*original_socket)(int, int, int);
+	original_socket = reinterpret_cast<decltype (original_socket)>(dlsym(RTLD_NEXT, "socket"));
+	int descriptor = original_socket(domain, type, protocol);
+	std::cout << "original socket return descriptor " << descriptor << std::endl;
 	if (descriptor != -1) {
 		m_udp_descriptors.emplace(std::make_pair(descriptor, c_turbosocket()));
 		m_udp_descriptors.at(descriptor).connect_as_client();
@@ -13,9 +18,10 @@ int c_sockfd_manager::add_udp_descriptor(int domain, int type, int protocol) {
 }
 
 int c_sockfd_manager::add_tcp_descriptor(int domain, int type, int protocol) {
-	if (domain != AF_INET6) throw std::invalid_argument("domain is not AF_INET6");
+	std::cout << "add tcp socket" << std::endl;
+	//if (domain != AF_INET6) throw std::invalid_argument("domain is not AF_INET6");
 	if (type != SOCK_STREAM) throw std::invalid_argument("type is not SOCK_STREAM");
-	int descriptor = socket(domain, type, protocol);
+	int descriptor = ::socket(domain, type, protocol);
 	if (descriptor != -1) {
 		m_tcp_descriptors.emplace(std::make_pair(descriptor, c_turbosocket()));
 		m_tcp_descriptors.at(descriptor).connect_as_client();
@@ -24,7 +30,7 @@ int c_sockfd_manager::add_tcp_descriptor(int domain, int type, int protocol) {
 }
 
 int c_sockfd_manager::close(int fd) {
-	int ret = close(fd);
+	int ret = ::close(fd);
 	if (ret == -1) return ret; // close error
 	if (m_tcp_descriptors.find(fd) != m_tcp_descriptors.end()) {
 		m_tcp_descriptors.erase(fd);
@@ -40,5 +46,5 @@ c_turbosocket &c_sockfd_manager::get_turbosocket_for_descriptor(int fd) {
 	} else if (m_udp_descriptors.find(fd) != m_udp_descriptors.end()) {
 		return m_udp_descriptors.at(fd);
 	}
-	throw std::invalid_argument("not found descriptor");
+	throw std::invalid_argument("not found descriptor " + std::to_string(fd));
 }
--- preload.cpp
+++ preload.cpp
@@ -1,32 +1,40 @@
 #include "c_sockfd_manager.hpp"
+#include <dlfcn.h>
+#include <iostream>
 #include <mutex>
 #include <sys/socket.h>
 
 static c_sockfd_manager sockfd_manager;
 
 int socket(int domain, int type, int protocol) noexcept {
+	std::cout << "fake socket()" << std::endl;
 	try {
-		if (type == SOCK_DGRAM && domain == AF_INET6)
+		if (type == SOCK_DGRAM && domain == AF_INET) // XXX AF_INET6
 			return sockfd_manager.add_udp_descriptor(domain, type, protocol);
-		else if (type == SOCK_STREAM && domain == AF_INET6) {
+		else if (type == SOCK_STREAM && domain == AF_INET) { // XXX AF_INET6
 			return  sockfd_manager.add_tcp_descriptor(domain, type, protocol);
 		}
 	} catch (const std::exception &) {
 		return -1;
 	}
 
-	return socket(domain, type, protocol);
+	int(*original_socket)(int, int, int);
+	original_socket = reinterpret_cast<decltype (original_socket)>(dlsym(RTLD_NEXT, "socket"));
+	return original_socket(domain, type, protocol);
 }
 
 int close(int fd) {
+	std::cout << "fake close" << std::endl;
 	return sockfd_manager.close(fd);
 }
 
 ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen) {
+
 	return 0;
 }
 
 ssize_t sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen) {
+	std::cout << "fake sendto" << std::endl;
 	void * m_buf = nullptr;
 	size_t m_buf_size = 0;
 	c_turbosocket &turbosock_ref = sockfd_manager.get_turbosocket_for_descriptor(sockfd);
