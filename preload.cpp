5dd432fdec2ac08ed7eb8bbf9427b238ce26eee9
Robert Oleksy <robertoleksy6@gmail.com>
2017-02-23 10:03:52 +0000

    original_sendto

--- preload.cpp
+++ preload.cpp
@@ -10,9 +10,10 @@ static c_sockfd_manager sockfd_manager;
 int socket(int domain, int type, int protocol) noexcept {
 	std::cout << "fake socket()" << std::endl;
 	try {
-		if (type == SOCK_DGRAM && domain == AF_INET6) // XXX AF_INET6
+		if (type == SOCK_DGRAM && domain == AF_INET6) {
 			return sockfd_manager.add_udp_descriptor(domain, type, protocol);
-		else if (type == SOCK_STREAM && domain == AF_INET6) { // XXX AF_INET6
+		}
+		else if (type == SOCK_STREAM && domain == AF_INET6) {
 			return  sockfd_manager.add_tcp_descriptor(domain, type, protocol);
 		}
 	} catch (const std::exception &) {
@@ -36,17 +37,23 @@ int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
 
 ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen) {
 
-	return 0;
+	return -1;
 }
 
 ssize_t sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen) {
 	std::cout << "fake sendto" << std::endl;
 	void * m_buf = nullptr;
 	size_t m_buf_size = 0;
-	c_turbosocket &turbosock_ref = sockfd_manager.get_turbosocket_for_descriptor(sockfd);
-	std::tie<void *, size_t>(m_buf, m_buf_size) = turbosock_ref.get_buffer_for_write();
-	if (len > m_buf_size) return -1;
-	std::memcpy(m_buf, buf, len);
-	turbosock_ref.send(len);
-	return len;
+	try {
+		c_turbosocket &turbosock_ref = sockfd_manager.get_turbosocket_for_descriptor(sockfd);
+		std::tie<void *, size_t>(m_buf, m_buf_size) = turbosock_ref.get_buffer_for_write();
+		if (len > m_buf_size) return -1;
+		std::memcpy(m_buf, buf, len);
+		turbosock_ref.send(len);
+		return len;
+	} catch (const std::invalid_argument &) {
+		int(*original_sendto)(int, const void *, size_t, int, const struct sockaddr *, socklen_t);
+		original_sendto = reinterpret_cast<decltype (original_sendto)>(dlsym(RTLD_NEXT, "sendto"));
+		return original_sendto(sockfd, buf, len, flags, dest_addr, addrlen);
+	}
 }
